a46af9de663373bed899d805f7a1f5a72cb22b59
jonybang <Jonybange@gmail.com>
2019-01-16 12:45:13 +0200

    refactor: separate to services

--- database/interface.ts
+++ database/interface.ts
@@ -1,5 +1,4 @@
 export default interface IExplorerDatabase {
     addOrUpdateContour(contourGeohashes: string[], spaceTokenId: number);
     getContoursByParentGeohash(geohash: string): Promise<[{contour: string[], spaceTokenId: number}]>;
-    getContoursByInnerGeohash(geohash: string): Promise<[{contour: string[], spaceTokenId: number}]>;
 }
