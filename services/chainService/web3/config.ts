27499133b765e3e8199e64f102938a1f37e84b09
jonybang <Jonybange@gmail.com>
2019-04-16 13:13:56 +0200

    fix config

--- services/chainService/web3/config.ts
+++ services/chainService/web3/config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-    'contractsConfigUrl': 'https://galt-<%= env %>-contracts.surge.sh/',
+    'contractsConfigUrl': 'https://galt-<%= env %>-app-contracts.surge.sh/',
     'env': 'local',
     'wsServer': 'ws://localhost:8546',
     'contractName': 'splitMerge'
