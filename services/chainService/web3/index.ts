ce5c671ffb196e9ab9b7d29f4193536526750f9b
jonybang <Jonybange@gmail.com>
2020-05-25 15:53:27 +0200

    exception for mainnet

--- services/chainService/web3/index.ts
+++ services/chainService/web3/index.ts
@@ -422,20 +422,20 @@ class ExplorerChainWeb3Service implements IExplorerChainService {
       result.map((cPoint) => {
         cPoint = cPoint.toString(10);
 
-        // log('cPoint', cPoint);
-        if(process.env.CONTRACTS_CONFIG === 'mainnet.json') {
-          const { geohash5, height } = galtUtils.geohash5zToGeohash5(cPoint);
-          heightsContour.push(height);
-          const geohash = galtUtils.numberToGeohash(geohash5);
-          geohashContour.push(geohash);
-          contractContour.push(galtUtils.contractPoint.encodeFromGeohash(geohash));
-        } else {
+        // // log('cPoint', cPoint);
+        // if(process.env.CONTRACTS_CONFIG === 'mainnet.json') {
+        //   const { geohash5, height } = galtUtils.geohash5zToGeohash5(cPoint);
+        //   heightsContour.push(height);
+        //   const geohash = galtUtils.numberToGeohash(geohash5);
+        //   geohashContour.push(geohash);
+        //   contractContour.push(galtUtils.contractPoint.encodeFromGeohash(geohash));
+        // } else {
           contractContour.push(cPoint);
           const { lat, lon, height } = galtUtils.contractPoint.decodeToLatLonHeight(cPoint);
           const geohash = galtUtils.geohash.extra.encodeFromLatLng(lat, lon, 12);
           geohashContour.push(geohash);
           heightsContour.push(height);
-        }
+        // }
       });
       return {
         geohashContour,
@@ -477,19 +477,19 @@ class ExplorerChainWeb3Service implements IExplorerChainService {
         cPoint = cPoint.toString(10);
 
         log('cPoint', cPoint);
-        if(process.env.CONTRACTS_CONFIG === 'mainnet.json') {
-          const { geohash5, height } = galtUtils.geohash5zToGeohash5(cPoint);
-          heightsContour.push(height);
-          const geohash = galtUtils.numberToGeohash(geohash5);
-          geohashContour.push(geohash);
-          contractContour.push(galtUtils.contractPoint.encodeFromGeohash(geohash));
-        } else {
+        // if(process.env.CONTRACTS_CONFIG === 'mainnet.json') {
+        //   const { geohash5, height } = galtUtils.geohash5zToGeohash5(cPoint);
+        //   heightsContour.push(height);
+        //   const geohash = galtUtils.numberToGeohash(geohash5);
+        //   geohashContour.push(geohash);
+        //   contractContour.push(galtUtils.contractPoint.encodeFromGeohash(geohash));
+        // } else {
           contractContour.push(cPoint);
           const { lat, lon, height } = galtUtils.contractPoint.decodeToLatLonHeight(cPoint);
           const geohash = galtUtils.geohash.extra.encodeFromLatLng(lat, lon, 12);
           geohashContour.push(geohash);
           heightsContour.push(height);
-        }
+        // }
       });
       const tokenType = (result.spaceTokenType || result.tokenType).toString(10);
       return {
