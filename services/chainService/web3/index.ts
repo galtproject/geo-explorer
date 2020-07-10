88e56c5e57d0ad61f52c217df4835000593faba1
jonybang <Jonybange@gmail.com>
2020-07-10 16:51:32 +0200

    fix getMediatorContract

--- services/chainService/web3/index.ts
+++ services/chainService/web3/index.ts
@@ -99,6 +99,7 @@ class ExplorerChainWeb3Service implements IExplorerChainService {
   callbackOnReconnect: any;
 
   pprCache: any = {};
+  mediatorCache: any = {};
   communityCache: any = {};
   tokenizableCache: any = {};
 
@@ -615,12 +616,12 @@ class ExplorerChainWeb3Service implements IExplorerChainService {
   }
 
   getMediatorContract(address, type) {
-    if(this.pprCache[address]) {
-      return this.pprCache[address];
+    if(this.mediatorCache[address]) {
+      return this.mediatorCache[address];
     }
 
     const mediatorContract = this.createContract(type === 'foreign' ? 'ppForeignMediator' : 'ppHomeMediator', address);
-    this.pprCache[address] = mediatorContract;
+    this.mediatorCache[address] = mediatorContract;
     return mediatorContract;
   }
 
@@ -704,7 +705,7 @@ class ExplorerChainWeb3Service implements IExplorerChainService {
       return null;
     }
     if(!contract || !contract.methods[method]) {
-      console.log('contract.methods', contract.methods);
+      // console.log('contract.methods', contract.methods);
       console.error('Method', method, ' not found in contract',  contract._address);
       return null;
     }
